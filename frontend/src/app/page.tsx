ca168bb9fa6b8585d9e4d1cddfa157f7b2701b88
johnstewart00 <stew.john560@gmail.com>
2025-02-12 08:58:13 -0700

    add front end structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import ContactList from "./components/contactList";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <div className="text-5xl m-4">Welcome to Personal Network Tracker</div>
       <div className="text-3xl m-4">Your key to success</div>
-      <div className="bg-gray-800 text-white hover:bg-gray-400 text-center mt-56 p-5 w-fit mx-auto rounded-lg cursor-pointer">
-        Get Started
+      <div className="flex justify-center">
+        <ContactList />
       </div>
     </div>
   );
