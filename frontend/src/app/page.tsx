bcd4c9d6775cd1ac701240ede0f4565da2bc20a9
johnstewart00 <stew.john560@gmail.com>
2025-03-01 16:47:47 -0700

    add login, signup and add contact screens

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,23 @@
+import Link from "next/link";
 import ContactList from "./components/contactList";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <div className="text-5xl m-4">Welcome to Personal Network Tracker</div>
-      <div className="text-3xl m-4">Your key to success</div>
-      <div className="flex justify-center">
-        <ContactList />
+
+      <div className="flex justify-center items-center">
+        <h2 className="text-5xl font-bold">Contacts</h2>
+        <div className="flex justify-end p-4 ml-12">
+          <Link
+            href="addContact"
+            className="px-6 py-3 rounded-lg shadow-md bg-blue-600 text-white text-lg font-semibold hover:bg-blue-700 transition duration-200"
+          >
+            Add Contact
+          </Link>
+        </div>
       </div>
+      <ContactList />
     </div>
   );
 }
