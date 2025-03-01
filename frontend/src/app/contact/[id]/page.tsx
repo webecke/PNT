0b6aa0bf43715392dfdf76e8ddde79b4c563a46a
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2025-03-01 09:40:23 -0700

    move timeline below profile info
    Allows timeline to fill the screen horizontally.

--- frontend/src/app/contact/[id]/page.tsx
+++ frontend/src/app/contact/[id]/page.tsx
@@ -15,20 +15,28 @@ const ContactDetail = () => {
   const timelineEvents = getMockTimeline(contact);
 
   return (
-    <div className="flex m-12 p-6 shadow-lg rounded-lg bg-white">
-      <div className="max-w-xs ">
-        <ProfileIcon src={contact.image} alt={contact.name} />
-        <h1 className="text-3xl font-bold mb-4">{contact.name}</h1>
-        <p className="text-gray-700 bg-gray-100 rounded-lg shadow-sm p-2">
-          {contact.notes}
-        </p>
-      </div>
-      <div className="ml-6">
-        <div className="mt-4">
+    <div className="flex m-12 p-6 shadow-lg rounded-lg bg-white flex-col">
+      <div className="flex flex-row">
+        {/* Profile pic */}
+        <div className="max-w-xs">
+          <ProfileIcon src={contact.image} alt={contact.name} />
+          <h1 className="text-3xl font-bold mb-4">{contact.name}</h1>
+          <p className="text-gray-700 bg-gray-100 rounded-lg shadow-sm p-2">
+            {contact.notes}
+          </p>
+        </div>
+
+        {/* Contact info */}
+        <div className="mt-4 max-w-xs">
           <h2 className="text-xl font-semibold">Contact Info</h2>
           <p>📞 {contact.phone}</p>
           <p>✉️ {contact.email}</p>
         </div>
+      </div>
+
+      <div className="ml-6">
+
+        {/* Timeline */}
         <div className="mt-4">
           <h2 className="text-xl font-semibold">Timeline</h2>
           <Timeline timelineEvents={timelineEvents} />
