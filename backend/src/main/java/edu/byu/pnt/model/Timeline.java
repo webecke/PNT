ef85913abf979e9dfc12028257e0cbd03472e870
ammonharps-git <ammonharps@gmail.com>
2025-03-03 14:34:27 -0700

    Converted timeline model class to record

--- backend/src/main/java/edu/byu/pnt/model/Timeline.java
+++ backend/src/main/java/edu/byu/pnt/model/Timeline.java
@@ -1,25 +1,3 @@
 package edu.byu.pnt.model;
 
-public class Timeline {
-    private final String id;
-    private final String ownerID;
-    private final Event[] events;
-
-    Timeline(String id, String ownerID, Event[] events) {
-        this.id = id;
-        this.ownerID = ownerID;
-        this.events = events;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getOwnerID() {
-        return ownerID;
-    }
-
-    public Event[] getEvents() {
-        return events;
-    }
-}
+public record Timeline(String id, String ownerID, Event[] events) { };
