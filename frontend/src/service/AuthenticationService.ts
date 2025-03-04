336dd3511057fe326eb9e5ba5a3c6adec3038def
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2025-03-03 23:11:11 -0700

    feat: add AuthenticationService.ts

---
+++ frontend/src/service/AuthenticationService.ts
@@ -0,0 +1,11 @@
+export default class AuthenticationService {
+  public async register(email: string, password: string) {
+    // TODO Contact server
+    return "FAKE-AUTH-TOKEN";
+  }
+
+  public async login(email: string, password: string) {
+    // TODO Contact server
+    return "FAKE-AUTH-TOKEN";
+  }
+}
