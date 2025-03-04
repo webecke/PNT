427f3acde5034f5d33a0133a6f52adb2eb70cab9
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2025-03-03 22:20:21 -0700

    feat: a few more utils

--- frontend/src/app/utils/stringUtils.ts
+++ frontend/src/app/utils/stringUtils.ts
@@ -1,3 +1,9 @@
 import { concatWithSeparator } from "@/app/utils/arrayUtils";
 
 export const formatStringList = strings => concatWithSeparator(strings, " | ", "");
+
+export const parseCommaSeparatedStringToList = (commaSeparatedString: string): string[] => {
+  return commaSeparatedString
+    .split(",")
+    .map(s => s.trim());
+};
