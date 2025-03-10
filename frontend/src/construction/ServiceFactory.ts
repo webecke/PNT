c44df64be0efd0d85a52c9ebd0628c010c22901b
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2025-03-10 12:57:24 -0700

    feat: add getAuthService() to ServiceFactory

--- frontend/src/construction/ServiceFactory.ts
+++ frontend/src/construction/ServiceFactory.ts
@@ -1,6 +1,16 @@
+import AuthServiceImpl from "@/service/AuthServiceImpl";
+import { AuthService } from "@/service/AuthService";
 import { ServerFactory } from "@/construction/ServerFactory";
 
-export class ServiceFactory {
+export interface AuthServiceFactory {
+  getAuthService(): AuthService
+}
+
+export class ServiceFactory implements AuthServiceFactory {
   constructor(private serverFactory: ServerFactory) {
+  }
+
+  public getAuthService(): AuthService {
+    return new AuthServiceImpl();
   }
 }
--- frontend/test/construction/AlmightySingleton.test.ts
+++ frontend/test/construction/AlmightySingleton.test.ts
@@ -1,10 +1,29 @@
-import { describe, it } from "@jest/globals";
+import { beforeEach, describe, it } from "@jest/globals";
 import { AlmightySingleton } from "@/construction/AlmightySingleton";
+import { ServiceFactory } from "@/construction/ServiceFactory";
+import { instance, mock } from "@typestrong/ts-mockito";
+import { ServerFactory } from "@/construction/ServerFactory";
+import { AuthService } from "@/service/AuthService";
 
 describe("The Almighty Singleton", () => {
   const singleton: AlmightySingleton = AlmightySingleton.getInstance();
 
   it("can create a ServiceFactory", () => {
     const serviceFactory: ServiceFactory = singleton.getServiceFactory();
+  });
+});
+
+describe("ServiceFactory", () => {
+  let serverFactoryMock: ServerFactory;
+  let serviceFactory: ServiceFactory;
+
+  beforeEach(() => {
+    serverFactoryMock = mock<ServerFactory>();
+    const server = instance(serverFactoryMock);
+    serviceFactory = new ServiceFactory(server);
+  });
+
+  it("can create an AuthService", () => {
+    const authService: AuthService = serviceFactory.getAuthService();
   });
 });
