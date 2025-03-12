88a6c44764e0652e82bcf60a3358955785d516d1
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2025-03-12 11:40:44 -0700

    eslint: "use" or remove unused variables in stubbed methods

--- frontend/src/service/AuthenticationService.ts
+++ frontend/src/service/AuthenticationService.ts
@@ -1,11 +1,11 @@
 export default class AuthenticationService {
   public async register(email: string, password: string) {
     // TODO Contact server
-    return "FAKE-AUTH-TOKEN";
+    return `FAKE-AUTH-TOKEN. Credentials: ${email} ${password}`;
   }
 
   public async login(email: string, password: string) {
     // TODO Contact server
-    return "FAKE-AUTH-TOKEN";
+    return `FAKE-AUTH-TOKEN. Credentials: ${email} ${password}`;
   }
 }
