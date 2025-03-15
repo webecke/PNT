39d44dc37c453cc00869fed989c7a8a4af4e0f87
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-03-15 17:17:45 -0600

    server facade category

--- frontend/src/service/server/ServerFacade.ts
+++ frontend/src/service/server/ServerFacade.ts
@@ -30,7 +30,24 @@ export class ServerFacade {
     }
   }
 
-
+  category = {
+    getCategory: async (categoryId: string) => {
+      const response = await this.communicator.get(`/categories/${categoryId}`);
+      return response;
+    },
+    deleteCategory: async (categoryId: string) => {
+      const response = await this.communicator.delete(`/categories/${categoryId}`);
+      return response;
+    },
+    addCategory: async (categoryData: any) => {
+      const response = await this.communicator.post('/categories', categoryData);
+      return response;
+    },
+    updateCategory: async (categoryId: string, categoryData: any) => {
+      const response = await this.communicator.post(`/categories/${categoryId}`, categoryData);
+      return response;
+    }
+  }
 }
 
 type AuthToken = {
