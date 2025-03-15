67e9bd7b8771abb45b406a1e7a1c49fd540c798f
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2025-03-15 17:14:37 -0600

    server facade auth

--- frontend/src/service/AuthenticationService.ts
+++ frontend/src/service/AuthenticationService.ts
@@ -1,11 +1,17 @@
+import { ServerFacade } from "@/service/server";
+import { AuthResponse } from "@/service/server/response/AuthResponse";
+
 export default class AuthenticationService {
+  private server = new ServerFacade();
+
   public async register(email: string, password: string) {
     // TODO Contact server
     return `FAKE-AUTH-TOKEN. Credentials: ${email} ${password}`;
   }
 
   public async login(email: string, password: string) {
-    // TODO Contact server
+    const response = await this.server.auth.login(email, password);
+    // TODO what happens now?
     return `FAKE-AUTH-TOKEN. Credentials: ${email} ${password}`;
   }
 }
---
+++ frontend/src/service/server/ServerFacade.ts
@@ -0,0 +1,39 @@
+/**
+ * ServerFacade
+ *
+ * A service that provides a simplified interface for all backend API interactions.
+ * Acts as a facade over the ServerCommunicator, organizing endpoints by domain
+ * and handling data transformation between API and application models.
+ */
+import { ServerCommunicator } from './ServerCommunicator';
+import { AuthResponse } from "@/service/server/response/AuthResponse";
+
+export class ServerFacade {
+  private communicator: ServerCommunicator;
+  private token: AuthToken | null = null;
+
+  constructor() {
+    // Initialize the communicator with environment-specific settings
+    this.communicator = new ServerCommunicator();
+  }
+
+  auth = {
+    login: async (username: string, password: string) => {
+      const response = await this.communicator.post<AuthResponse>('/auth/login', { username, password })
+      // TODO: Handle the response and store the token
+    },
+
+    logout: async () => {
+      const response = await this.communicator.post<AuthResponse>('/auth/login', this.token)
+      this.token = null;
+      // TODO: Handle the response
+    }
+  }
+
+
+}
+
+type AuthToken = {
+  token: string;
+  username: string;
+}
