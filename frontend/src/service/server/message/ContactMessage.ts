84a7d47d9d3d625585f74db98949ec1763ff099c
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2025-04-04 10:13:03 -0700

    refactor: rename ContactMessage properties to match Contact

--- frontend/src/service/server/message/ContactMessage.ts
+++ frontend/src/service/server/message/ContactMessage.ts
@@ -6,7 +6,7 @@ export interface AddContactRequest {
   lastName: string;
   email: string;
   phone: string;
-  note: string;
+  notes: string;
 }
 
 export interface GetContactResponse extends BasicResponse {
