2f505d02fba44b7260b34a6769f20d20ac1d37ad
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2025-04-14 13:01:11 -0700

    add demo data

--- frontend/src/components/event/Schedule.tsx
+++ frontend/src/components/event/Schedule.tsx
@@ -1,8 +1,38 @@
 import { getMockTimeline } from "@/utils/mockTimelineEvents";
 import Timeline from "@/components/timeline/Timeline";
+import ContactService from "@/service/ContactService";
+import EventService from "@/service/EventService";
+import { Contact } from "@/model/Contact";
+import { TimelineEvent } from "@/model/TimelineEvent";
+import { hardcodedContactIds, hardcodedEventIds } from "@/utils/mockContacts";
+import { useEffect } from "react";
+import { useUserContext } from "@/contexts/user-context";
 
 const Schedule = () => {
   const timelineEvents = getMockTimeline();
+  const {setContacts, setEvents} = useUserContext();
+
+  const fakeData = async () => {
+    const contactService: ContactService = new ContactService();
+    const eventService: EventService = new EventService();
+    const contactsList: Contact[] = [];
+    const eventsList: TimelineEvent[] = [];
+    for (const contactId of hardcodedContactIds) {
+      const contact: Contact = await contactService.getContact(contactId);
+      contactsList.push(contact);
+    }
+    for (const eventId of hardcodedEventIds) {
+      const timelineEvent: TimelineEvent = await eventService.getEvent(eventId);
+      eventsList.push(timelineEvent);
+    }
+    setEvents(eventsList);
+    setContacts(contactsList);
+  }
+
+  useEffect(() => {
+    fakeData();
+  }, []);
+
 
   return (
     <div className="m-12 p-6 shadow-lg rounded-lg bg-white">
